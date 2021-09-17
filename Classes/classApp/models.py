3a39044fa158d99de6e22d0c0b2c4be525a41777
codhharris713 <83795096+codhharris713@users.noreply.github.com>
2021-09-16 23:00:18 -0700

    django challenge

---
+++ Classes/classApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class djangoClasses (models.Model):
+    title = models.CharField(max_length=60)
+    course_number = models.IntegerField()
+    instructor_name = models.CharField(max_length=60)
+    duration = models.FloatField()
+
+    object = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+
